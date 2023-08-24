ee6bfbf09d583110c5da3c6e34b659b4ab9b4e6c
Joshua Cruz <70266989+Juicewa-create@users.noreply.github.com>
2023-08-24 15:43:56 +0800

    Add files via upload

---
+++ BankAccount.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySuperBank
+{
+    public class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in _allTransactions)
+                {
+                    balance += item.Amount;
+                }
+
+                return balance;
+            }
+        }
+
+        private static int s_accountNumberSeed = 0729010117;
+
+        private List<Transaction> _allTransactions = new List<Transaction>();
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            Number = s_accountNumberSeed.ToString();
+            s_accountNumberSeed++;
+
+            Owner = name;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial Balance");
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            _allTransactions.Add(deposit);
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            _allTransactions.Add(withdrawal);
+        }
+        public string GetAccountHistory()
+        {
+            var report = new System.Text.StringBuilder();
+
+            decimal balance = 0;
+            report.AppendLine("\nDate\t\tAmount\t\tBalance\t\tNote");
+            foreach (var item in _allTransactions)
+            {
+                balance += item.Amount;
+                report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t\t{balance}\t\t{item.Notes}");
+            }
+
+            return report.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace MySuperBank
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            char pesoSymbol = '\u20B1';
+
+            var account = new BankAccount("Joshua", 10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {pesoSymbol}{account.Balance} initial balance.");
+
+            account.MakeWithdrawal(7000, DateTime.Now, "Smartphone");
+            Console.WriteLine($"\nYou paid {pesoSymbol}7000 for Smartphone");
+            Console.WriteLine($"Your Current Account Balance is: {pesoSymbol}{account.Balance}");
+            
+            account.MakeDeposit(2500, DateTime.Now, "Allowance");
+            Console.WriteLine($"\nYou've Deposited {pesoSymbol}2500");
+            Console.WriteLine($"Your Current Account Balance is: {pesoSymbol}{account.Balance}");
+
+            account.MakeWithdrawal(4000, DateTime.Now, "Table");
+            Console.WriteLine($"\nYou paid {pesoSymbol}4000 for Table");
+            Console.WriteLine($"Your Current Account Balance is: {pesoSymbol}{account.Balance}");
+
+            account.MakeDeposit(2500, DateTime.Now, "Savings");
+            Console.WriteLine($"\nYou've Deposited {pesoSymbol}2500");
+            Console.WriteLine($"Your Current Account Balance is: {pesoSymbol}{account.Balance}");
+
+            account.MakeWithdrawal(1000, DateTime.Now, "Shirt");
+            Console.WriteLine($"\nYou paid {pesoSymbol}1000 for Shirt");
+            Console.WriteLine($"Your Current Account Balance is: {pesoSymbol}{account.Balance}");
+
+
+            Console.WriteLine(account.GetAccountHistory());
+        }
+    }
+}
